20ec5bf36f6664067e9b63b3c525090b48b704be
abhinavginti <abhinavginti@gmail.com>
2020-11-16 08:44:41 +0530

    Updates

---
+++ precache-manifest.c4a6c7b134ae5e963988e814dc6d9e84.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7075e9beb7b60a13077efe1f8879f73",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "d43afdc54b2dc61079c3",
+    "url": "/Personal-Portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "314b90a8e7285ba5a580",
+    "url": "/Personal-Portfolio/static/css/main.440f9ef6.chunk.css"
+  },
+  {
+    "revision": "d43afdc54b2dc61079c3",
+    "url": "/Personal-Portfolio/static/js/2.8102ac14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Personal-Portfolio/static/js/2.8102ac14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314b90a8e7285ba5a580",
+    "url": "/Personal-Portfolio/static/js/main.1fcf896a.chunk.js"
+  },
+  {
+    "revision": "c595f0caa5a61d14ecd0",
+    "url": "/Personal-Portfolio/static/js/runtime-main.4a0972e9.js"
+  },
+  {
+    "revision": "20f45afcc25432454b64e3293b1bafea",
+    "url": "/Personal-Portfolio/static/media/0.20f45afc.jfif"
+  },
+  {
+    "revision": "f7169c2c0be2a4aa5522514687c24629",
+    "url": "/Personal-Portfolio/static/media/1.f7169c2c.png"
+  },
+  {
+    "revision": "3feb940b7d20f1501a49cfd9df6be0e5",
+    "url": "/Personal-Portfolio/static/media/2.3feb940b.png"
+  },
+  {
+    "revision": "6eedd1f1d3411cef977ebe31a13f5853",
+    "url": "/Personal-Portfolio/static/media/3.6eedd1f1.png"
+  },
+  {
+    "revision": "8c00e4992240b6ed3df5e308b0aae924",
+    "url": "/Personal-Portfolio/static/media/video.8c00e499.mp4"
+  }
+]);
